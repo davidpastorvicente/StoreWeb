58304808a08c1a6d68e91377b945bee68ea851e9
David <davidpastorvicente@gmail.com>
2020-08-17 23:57:55 +0200

    Add Product files

--- src/app/app.js
+++ src/app/app.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './app.css';
+import Product from '../product/product';
 import Http from '../services/http';
 
-const http = new Http();
+class App extends Component {
 
-function App() {
-  http.getProducts().then(data => console.log(data));
+  constructor(props) {
+    super(props);
+    this.state = {products:[]};
+    this.loadData = this.loadData.bind(this);
+    this.productList = this.productList.bind(this);
+    this.loadData();
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/app.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  loadData = () => {
+    const http = new Http();
+    var self = this;
+    http.getProducts().then(data => self.setState({products: data}));
+  }
+
+  productList = () => {
+    const list = this.state.products.map(product =>
+      <div className="col-sm-4" key={product._id}>
+        <Product title={product.title} price={product.price} url={product.url} />
+      </div>
+    );
+
+    return (list);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="container App-main">
+          <div className="row">
+            {this.productList()}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
