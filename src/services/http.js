f58d0867c0f641e8a8452dc96872fad6d72aa737
David <davidpastorvicente@gmail.com>
2020-08-19 13:20:45 +0200

    Add GET wishlists

--- src/services/http.js
+++ src/services/http.js
@@ -1,12 +1,25 @@
 import 'whatwg-fetch';
 
+let instance = null;
+
 class Http {
+  constructor() {
+    if(!instance) instance = this;
+    return instance;
+  }
+
   getProducts = () => {
     var promise = new Promise((resolve, reject) => {
       fetch('http://localhost:3001/product')
-      .then(response => {
-        resolve(response.json());
-      })
+      .then(response => resolve(response.json()))
+    });
+    return promise;
+  }
+
+  getWishlist = () => {
+    var promise = new Promise((resolve, reject) => {
+      fetch('http://localhost:3001/wishlist')
+      .then(response => resolve(response.json()))
     });
     return promise;
   }
--- src/wishlist/wishlist.js
+++ src/wishlist/wishlist.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import './wishlist.css';
 import Data from '../services/data';
-import Notification, {NOTIF_WCHANGE} from '../services/notification'
+import Notification, {NOTIF_WCHANGE} from '../services/notification';
+import Http from '../services/http';
 import Miniproduct from '../miniproduct/miniproduct';
 
 let notif = new Notification();
+let http = new Http();
+let data = new Data();
 
 class Wishlist extends Component {
   constructor(props) {
     super(props);
     this.state = {wishlist: []};
+    this.initWishlist = this.initWishlist.bind(this);
     this.createWishlist = this.createWishlist.bind(this);
     this.modifyWishlist = this.modifyWishlist.bind(this);
+    this.initWishlist();
   }
 
   componentDidMount() {
@@ -20,6 +25,14 @@ class Wishlist extends Component {
 
   componentWillUnmount() {
     notif.remove(NOTIF_WCHANGE, this);
+  }
+
+  initWishlist = () => {
+    http.getWishlist().then(wishs => {
+      this.setState({wishlist: wishs[0].products});
+      for(var x=0; x<this.state.wishlist.length; x++)
+        data.add(this.state.wishlist[x]);
+    });
   }
 
   createWishlist = () => {
